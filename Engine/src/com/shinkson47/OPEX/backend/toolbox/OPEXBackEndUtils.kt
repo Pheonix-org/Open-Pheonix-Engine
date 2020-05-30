637e99cd9140e9730ce6cdc3513a57ae6c6c20d6
Tyler <Jdngray77@icloud.com>
2020-05-30 12:28:20 +0100

    Back end utils
    Converted back end utils to kotlin.
    Commented class.

--- Engine/src/com/shinkson47/OPEX/backend/toolbox/OPEXBackEndUtils.kt
+++ Engine/src/com/shinkson47/OPEX/backend/toolbox/OPEXBackEndUtils.kt
@@ -1,41 +1,37 @@
-package com.shinkson47.OPEX.backend.toolbox;
+package com.shinkson47.OPEX.backend.toolbox
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import com.shinkson47.OPEX.backend.errormanagement.EMSHelper
+import com.shinkson47.OPEX.backend.errormanagement.exceptions.OPEXStaticException
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.io.UnsupportedEncodingException
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
 
-import com.shinkson47.OPEX.backend.errormanagement.EMSHelper;
-import com.shinkson47.OPEX.backend.errormanagement.exceptions.OPEXStaticException;
-import com.shinkson47.OPEX.backend.runtime.threading.OPEXGame;
+/**
+ * Container for general tool methods and functions
+ * //TODO these should have thier own classes.
+ */
 
-public class OPEXBackEndUtils {
+@Throws(IllegalAccessException::class, IllegalArgumentException::class, InvocationTargetException::class, NoSuchMethodException::class, SecurityException::class)
+fun getAndInvokeMethod(cls: Class<*>, MethodName: String?, ParameterTypes: Class<*>?, parameters: Any?, caller: Any?) {
+            invokeMethod(cls.getMethod(MethodName, ParameterTypes), caller, parameters)
+        }
 
-	private OPEXBackEndUtils() throws OPEXStaticException {
-		throw new OPEXStaticException("Back End Tools is not instantiable!");
-	}
-
-	public static void getAndInvokeMethod(Class<?> cls, String MethodName, Class<?> ParameterTypes, Object parameters,
-			Object caller) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException,
-			NoSuchMethodException, SecurityException {
-		invokeMethod(cls.getMethod(MethodName, ParameterTypes), caller, parameters);
-	}
-
-	public static void invokeMethod(Method mtd, Object caller, Object parameters)
-			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		mtd.invoke(caller, parameters);
-	}
-
-	public static void ParseToSystemIn(String data){
-		InputStream old = System.in;
-		try {
-			InputStream testInput = new ByteArrayInputStream( data.getBytes("UTF-8") );
-			System.setIn(testInput);
-		} catch (UnsupportedEncodingException e) {
-			EMSHelper.handleException(e);
-		} finally {
-			System.setIn(old);
-		}
-	}
+@Throws(IllegalAccessException::class, IllegalArgumentException::class, InvocationTargetException::class)
+fun invokeMethod(mtd: Method, caller: Any?, parameters: Any?) {
+    mtd.invoke(caller, parameters)
 }
+
+fun ParseToSystemIn(data: String) {
+    val old = System.`in`
+    try {
+        val testInput: InputStream = ByteArrayInputStream(data.toByteArray(charset("UTF-8")))
+        System.setIn(testInput)
+    } catch (e: UnsupportedEncodingException) {
+        EMSHelper.handleException(e)
+    } finally {
+        System.setIn(old)
+    }
+}
+
